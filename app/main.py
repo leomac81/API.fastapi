312e8c51ad913675d1de8a8a94b642efce275852
Leo <leomacintyre81@gmail.com>
2023-04-24 22:22:09 +0100

    gameoflife

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.testclient import TestClient
 from . import models
 from .database import engine
@@ -8,6 +8,9 @@ from .routers import post, user, auth, vote
 from pydantic import BaseSettings
 from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
 #models.Base.metadata.create_all(bind=engine)
@@ -24,14 +27,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-@app.get("/")
-def root():
-    return {"message": "fixed it myself"}
+@app.get("/", response_class = HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 
---
+++ static/script.js
@@ -0,0 +1,97 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const cellSize = 10;
+const gridSize = 100;
+let grid = generateRandomGrid(gridSize);
+
+document.getElementById('startButton').addEventListener('click', startGame);
+
+function startGame() {
+    let iterations = 0;
+    let unchangedIterations = 0;
+    let lastAliveCells = -1;
+    const gameInterval = setInterval(() => {
+        drawGrid(grid);
+        const newGrid = getNextGrid(grid);
+        const aliveCells = countAliveCells(newGrid);
+
+        if (aliveCells === lastAliveCells) {
+            unchangedIterations++;
+        } else {
+            unchangedIterations = 0;
+        }
+
+        if (iterations >= 10 && unchangedIterations >= 10) {
+            clearInterval(gameInterval);
+        } else {
+            grid = newGrid;
+            lastAliveCells = aliveCells;
+            iterations++;
+        }
+    }, 100);
+}
+
+function drawGrid(grid) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    grid.forEach((row, i) => {
+        row.forEach((cell, j) => {
+            ctx.fillStyle = cell ? 'black' : 'white';
+            ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+            ctx.strokeStyle = 'lightgray';
+            ctx.strokeRect(j * cellSize, i * cellSize, cellSize, cellSize);
+        });
+    });
+}
+
+function generateRandomGrid(size) {
+    return Array.from({ length: size }, () =>
+        Array.from({ length: size }, () => Math.random() >= 0.5)
+    );
+}
+
+function getNextGrid(grid) {
+    return grid.map((row, i) =>
+        row.map((cell, j) => {
+            const aliveNeighbors = countAliveNeighbors(grid, i, j);
+            if (cell) {
+                return aliveNeighbors === 2 || aliveNeighbors === 3;
+            } else {
+                return aliveNeighbors === 3;
+            }
+        })
+    );
+}
+
+function countAliveNeighbors(grid, i, j) {
+    const directions = [
+        [-1, -1], [-1, 0], [-1, 1],
+        [0, -1], [0, 0],
+        [0, 1],
+        [1, -1], [1, 0], [1, 1],
+    ];
+
+    let count = 0;
+
+    directions.forEach(([di, dj]) => {
+        const ni = i + di;
+        const nj = j + dj;
+
+        if (ni >= 0 && ni < gridSize && nj >= 0 && nj < gridSize) {
+            count += grid[ni][nj] ? 1 : 0;
+        }
+    });
+
+    return count;
+}
+
+function countAliveCells(grid) {
+    let count = 0;
+
+    grid.forEach(row => {
+        row.forEach(cell => {
+            if (cell) count++;
+        });
+    });
+
+    return count;
+}
