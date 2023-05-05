2e24ae5cb6451df37ccba6591797ceaad1b7535a
Leo <leomacintyre81@gmail.com>
2023-05-05 15:42:38 +0100

    display posts

--- static/script.js
+++ static/script.js
@@ -2,17 +2,39 @@ function redirectToApiDocs() {
     window.location.href = 'https://www.leoapi.xyz/docs';
   }
   
-// Fetch the posts data from the API
-async function fetchPosts() {
-    const response = await fetch('/posts/');
+// Authenticate the user and get the access token
+async function login(username, password) {
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+  
+    if (!response.ok) {
+      throw new Error('Login failed');
+    }
+  
+    const data = await response.json();
+    return data.access_token;
+  }
+  
+  // Fetch the posts data from the API
+  async function fetchPosts(accessToken) {
+    const response = await fetch('/posts/', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    });
     const posts = await response.json();
     return posts;
   }
   
   // Render the list of posts on the webpage
-  async function renderPosts() {
+  async function renderPosts(accessToken) {
     const postsContainer = document.getElementById('posts-container');
-    const posts = await fetchPosts();
+    const posts = await fetchPosts(accessToken);
   
     posts.forEach((post) => {
       const postElement = document.createElement('div');
@@ -30,6 +52,19 @@ async function fetchPosts() {
     });
   }
   
-  // Call the renderPosts function when the page loads
-  renderPosts();
+  // Handle the login form submission
+  document.getElementById('login-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    try {
+      const accessToken = await login(username, password);
+      await renderPosts(accessToken);
+    } catch (error) {
+      alert('Login failed. Please check your credentials and try again.');
+    }
+  });
+  
   
