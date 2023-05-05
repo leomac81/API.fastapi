419ce1d75884778084fb3a7fb46145dc820103eb
Leo <leomacintyre81@gmail.com>
2023-05-05 17:39:34 +0100

    posts not displaying

--- static/script.js
+++ static/script.js
@@ -77,6 +77,7 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
   });
   
   function displayPosts(posts) {
+    document.getElementById('login-form').style.display = 'none';
     console.log('Displaying posts:', posts);
     const postsContainer = document.getElementById('posts-container');
     postsContainer.innerHTML = '';
@@ -99,4 +100,79 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
       postsContainer.appendChild(postDiv);
     }
   }
+
+
+  // Add these functions for each API call
+
+async function createUser(user) {
+    const response = await fetch('/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to create user');
+    }
+  
+    const newUser = await response.json();
+    console.log('Created user:', newUser);
+    return newUser;
+  }
+  
+  async function getUser(id) {
+    const accessToken = localStorage.getItem('access_token');
+    const response = await fetch(`/users/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to get user');
+    }
+  
+    const user = await response.json();
+    console.log('Fetched user:', user);
+    return user;
+  }
+  
+  // Update the login form submission handling
+  document.getElementById('login-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+  
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    try {
+      const accessToken = await login(email, password);
+      const posts = await fetchPosts(accessToken);
+      displayPosts(posts);
+  
+      // Hide login form and display the new buttons
+      document.getElementById('login-form').classList.add('hidden');
+      document.getElementById('create-post-button').classList.remove('hidden');
+      document.getElementById('get-user-button').classList.remove('hidden');
+      document.getElementById('create-user-button').classList.remove('hidden');
+      document.getElementById('vote-button').classList.remove('hidden');
+    } catch (error) {
+      alert('Login failed. Please check your credentials and try again.');
+    }
+  });
+  
+  // Add the display functions for forms
+  function displayCreatePostForm() {
+    // Display the form for creating a new post
+  }
+  
+  function displayCreateUserForm() {
+    // Display the form for creating a new user
+  }
+  
+  function displayVoteForm() {
+    // Display the form for voting
+  }
+  
   
