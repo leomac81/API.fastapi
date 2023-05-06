93213ecd195c47300376662a6de5c5dbf8ecdc07
Leo <leomacintyre81@gmail.com>
2023-05-06 20:17:46 +0100

    adding post buttons

--- static/script.js
+++ static/script.js
@@ -71,6 +71,7 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
       const accessToken = await login(email, password);
       const posts = await fetchPosts(accessToken);
       displayPosts(posts);
+      showUserActions(); // Add this line
     } catch (error) {
       alert('Login failed. Please check your credentials and try again.');
     }
@@ -78,6 +79,7 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
   
   function displayPosts(posts) {
     document.getElementById('login-form').style.display = 'none';
+    document.getElementById('post-actions').style.display = 'block';
     console.log('Displaying posts:', posts);
     const postsContainer = document.getElementById('posts-container');
     postsContainer.innerHTML = '';
@@ -99,6 +101,10 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
   
       postsContainer.appendChild(postDiv);
     }
+    // Add event listeners for user actions here
+  showUserActions();
+
+  
   }
 
   async function createUser(email, password) {
@@ -134,4 +140,109 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
     }
   });
   
+  function showUserActions() {
+    const userActions = document.getElementById('user-actions');
+    userActions.style.display = 'block';
+  }
   
+// Create Post
+async function createPost(title, content, accessToken) {
+  const response = await fetch('/posts/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ title, content }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to create post');
+  }
+
+  const post = await response.json();
+  return post;
+}
+
+// Update Post
+async function updatePost(postId, title, content, accessToken) {
+  const response = await fetch(`/posts/${postId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ title, content }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to update post');
+  }
+
+  const post = await response.json();
+  return post;
+}
+
+// Get Post
+async function getPost(postId, accessToken) {
+  const response = await fetch(`/posts/${postId}`, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch post');
+  }
+
+  const post = await response.json();
+  return post;
+}
+
+// Delete Post
+async function deletePost(postId, accessToken) {
+  const response = await fetch(`/posts/${postId}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to delete post');
+  }
+}
+
+
+document.getElementById('create-post-btn').addEventListener('click', async () => {
+  const title = document.getElementById('create-title').value;
+  const content = document.getElementById('create-content').value;
+  await createPost(title, content);
+  const accessToken = localStorage.getItem('access_token');
+  const posts = await fetchPosts(accessToken);
+  displayPosts(posts);
+});
+
+document.getElementById('update-post-btn').addEventListener('click', async () => {
+  const postId = document.getElementById('update-post-id').value;
+  const title = document.getElementById('update-title').value;
+  const content = document.getElementById('update-content').value;
+  await updatePost(postId, title, content);
+  const accessToken = localStorage.getItem('access_token');
+  const posts = await fetchPosts(accessToken);
+  displayPosts(posts);
+});
+
+document.getElementById('get-post-btn').addEventListener('click', async () => {
+  const postId = document.getElementById('get-post-id').value;
+  const post = await getPost(postId);
+  alert(`Title: ${post.title}\nContent: ${post.content}`);
+});
+
+document.getElementById('delete-post-btn').addEventListener('click', async () => {
+  const postId = document.getElementById('delete-post-id').value;
+  await deletePost(postId);
+  const accessToken = localStorage.getItem('access_token');
+  const posts = await fetchPosts(accessToken);
+  displayPosts(posts);
+});
