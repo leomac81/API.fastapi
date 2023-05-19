71f424cb33fcb6d09dbd083a10fbe4afbc116f8a
Leo <leomacintyre81@gmail.com>
2023-05-19 22:42:45 +0100

    addign tasks

--- app/routers/task.py
+++ app/routers/task.py
@@ -8,10 +8,10 @@ router = APIRouter(
     tags=["Tasks"],
 )
 
-get_db = database.get_db
+#get_db = database.get_db
 
 @router.post("/", response_model=schemas.Task)
-def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     db_task = models.Task(**task.dict(), user_id = current_user.id)
     db.add(db_task)
     db.commit()
@@ -19,7 +19,7 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db), current
     return db_task
 
 @router.delete("/{task_id}")
-def delete_task(task_id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+def delete_task(task_id: int, db: Session = Depends(get_db), current_user: int= Depends(oauth2.get_current_user)):
     db_task = db.query(models.Task).filter(models.Task.id == task_id, models.Task.user_id == current_user.id).first()
     if not db_task:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
