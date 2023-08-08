770ba7dfdd46547ffc596411b6cb206600ca54dd
Leo <leomacintyre81@gmail.com>
2023-08-08 20:02:41 +0100

    front and backend cleanup, added public habits

--- app/routers/habits.py
+++ app/routers/habits.py
@@ -1,17 +1,11 @@
-from fastapi import FastAPI, Body, Response, status, HTTPException, Depends, APIRouter
+from fastapi import  Response, status, HTTPException, Depends, APIRouter
 from typing import List
-from sqlalchemy.orm import Session
-from fastapi import status, Depends
 from sqlalchemy.orm import Session, joinedload
 from app.database import get_db
-from fastapi import status, Depends
 from sqlalchemy.orm import Session
 from app import models, schemas, oauth2
 from app.database import get_db
-from app import models, schemas, utils
 from pydantic import EmailStr
-from sqlalchemy.exc import IntegrityError
-import datetime
 from datetime import timedelta, date
 
 router = APIRouter(
@@ -20,38 +14,19 @@ router = APIRouter(
 )
 
 
-@router.post("/create_habit/", status_code=status.HTTP_201_CREATED, response_model=schemas.Habit)
-async def create_habit(habit: schemas.HabitCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Habit)
+async def create_habit(habit: schemas.HabitCreate, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
 
     new_habit = models.Habits(owner_id=current_user.id, **habit.dict())
-    try:
-        db.add(new_habit)
-        db.commit()
-        db.refresh(new_habit)
-
-        # Add HabitCompletion records for daily habits
-        if habit.frequency == 'daily':
-            end_date = habit.end_date
-              # Assuming end_date is a date object; if it's a string, you'll need to parse it into a date
-            current_date = date.today()
-
-            while current_date <= end_date:
-                new_completion = models.HabitCompletion(
-                    date=current_date, completed=False, habit_id=new_habit.id)
-                db.add(new_completion)
-                current_date += timedelta(days=1)
-
-            db.commit()
-
-    except IntegrityError:
-        db.rollback()  # roll back the transaction
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Failed to create new habit. This may be because of a database integrity error.")
+    
+    db.add(new_habit)
+    db.commit()
     db.refresh(new_habit)
+
     return new_habit
 
 
-@router.get("/{user_email}", response_model=List[schemas.Habit])
+@router.get("/email/{user_email}", response_model=List[schemas.Habit])
 async def read_user_habits_email(user_email: EmailStr,
                                  db: Session = Depends(get_db),
                                  current_user: models.User = Depends(oauth2.get_current_user)):
@@ -61,7 +36,6 @@ async def read_user_habits_email(user_email: EmailStr,
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    past_date = datetime.datetime.now() - timedelta(days=14)
     if current_user.email == user_email:
         habits = db.query(models.Habits).options(joinedload(models.Habits.completions)).filter(
             models.Habits.owner_id == user.id).order_by(models.Habits.created_at).all()
@@ -82,15 +56,15 @@ async def create_habit_completion(habit_id: int,
         raise HTTPException(status_code=404, detail="Habit not found")
 
     if habit.frequency == models.Frequency.DAILY:
-        period_start = datetime.date.today()
+        period_start = date.today()
     elif habit.frequency == models.Frequency.WEEKLY:
-        period_start = datetime.date.today() - timedelta(days=habit_completion.date.weekday())
+        period_start = date.today() - timedelta(days=habit_completion.date.weekday())
     elif habit.frequency == models.Frequency.MONTHLY:
-        period_start = datetime.date.today().replace(day=1)
+        period_start = date.today().replace(day=1)
 
     existing_completion = db.query(models.HabitCompletion).filter(
         models.HabitCompletion.date >= period_start,
-        models.HabitCompletion.date <= datetime.date.today(),
+        models.HabitCompletion.date <= date.today(),
         models.HabitCompletion.habit_id == habit_id
     ).first()
 
@@ -99,16 +73,26 @@ async def create_habit_completion(habit_id: int,
             raise HTTPException(
                 status_code=409, detail=f"A completion for this habit already exists for this {habit.frequency} period, starting on {period_start}.")
         else:
-            existing_completion.date = datetime.date.today()
+            existing_completion.date = date.today()
             existing_completion.comment = habit_completion.comment
             existing_completion.completed = habit_completion.completed
             db.commit()
             db.refresh(existing_completion)
             return existing_completion
-
+    else:
+        new_completion = models.HabitCompletion(
+            habit_id=habit_id,
+            date=date.today(),
+            comment=habit_completion.comment,
+            completed=habit_completion.completed
+        )
+        db.add(new_completion)
+        db.commit()
+        db.refresh(new_completion)
+        return new_completion
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_habit(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def delete_habit(id: int, db: Session = Depends(get_db), current_user: models.User  = Depends(oauth2.get_current_user)):
 
     habit_query = db.query(models.Habits).filter(models.Habits.id == id)
     habit = habit_query.first()
@@ -125,8 +109,8 @@ def delete_habit(id: int, db: Session = Depends(get_db), current_user: int = Dep
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.get("/gethabits/{habit_id}", response_model=schemas.Habit)
-async def read_habit(habit_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+@router.get("/{habit_id}", response_model=schemas.Habit)
+async def read_habit(habit_id: int, db: Session = Depends(get_db), current_user: models.User  = Depends(oauth2.get_current_user)):
     habit = db.query(models.Habits).filter(
         models.Habits.id == habit_id).first()
 
@@ -139,3 +123,15 @@ async def read_habit(habit_id: int, db: Session = Depends(get_db), current_user:
                             detail="Not authorized to perform this action")
 
     return habit
+
+
+@router.get("/public/all", response_model=List[schemas.Habit])
+async def read_public_habits(db: Session = Depends(get_db),
+                             current_user: models.User = Depends(oauth2.get_current_user)):
+    current_date = date.today()
+    completions_subquery = db.query(models.HabitCompletion).filter(models.HabitCompletion.date <= current_date).subquery()
+
+    habits = db.query(models.Habits).outerjoin(completions_subquery, completions_subquery.c.habit_id == models.Habits.id).filter(
+        models.Habits.public == 'yes').order_by(models.Habits.created_at.desc()).limit(10).all()
+
+    return habits
