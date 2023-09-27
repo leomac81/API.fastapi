adb7c5113cbfa05c982cf1ebc1c62acfb3d9f5e7
Leo <leomacintyre81@gmail.com>
2023-09-27 16:49:00 +0100

    cleaned up main.py to problem solve

--- app/main.py
+++ app/main.py
@@ -10,13 +10,9 @@ from fastapi.templating import Jinja2Templates
 
 
 pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
-#models.Base.metadata.create_all(bind=engine)
-#initialises app and assigns it to app
 app = FastAPI()
 
-#defines all websites that will be allowed to interact with the fasapi application through CORS
 origins = ["https://www.google.com","http://127.0.0.1:8000","http://127.0.0.1:3000"]
-#The following block of code adds a middleware to the FastAPI application, allowing CORS for the specified origins, and allowing any methods and headers:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -24,19 +20,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#This line mounts a directory named "static" to serve static files (e.g., images, CSS, JavaScript) under the "/static" path.
-#app.mount("/static", StaticFiles(directory="static"), name="static")
-#This line initializes a Jinja2Templates instance to manage templates located in the "templates" directory.
-#templates = Jinja2Templates(directory="templates")
 
-#The following lines include routers from different modules into the main FastAPI application:
 app.include_router(habits.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-#The last block of code defines a root route ("/") that responds with an HTML file "index.html" using the Jinja2 template engine:
-#This route takes a Request object as a parameter and returns an HTMLResponse with the rendered "index.html" template.
-@app.get("/", response_class = HTMLResponse)
-async def root(request: Request):
+@app.get("/")
+async def root():
     return {"message": "Hello World pushing out to ubuntu"}
-    #return templates.TemplateResponse("index.html", {"request": request})
 
