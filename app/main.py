5d97134714b45f01579f1fc605fc063dba44678b
Leo <leomacintyre81@gmail.com>
2023-10-11 20:35:04 +0100

    added basic loign form

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from fastapi.templating import Jinja2Templates
 pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
 app = FastAPI()
 
-origins = ["https://www.leoapi.xyz","http://127.0.0.1:8000","http://127.0.0.1:3000", "http://localhost:3000"]
+origins = ["https://www.leoapi.xyz","http://127.0.0.1:8000","http://127.0.0.1:3000", "http://localhost:3000", "http://127.0.0.1:8000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
--- habits-app/src/App.js
+++ habits-app/src/App.js
@@ -1,16 +1,14 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/LoginForm';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
+      <Routes>
+        <Route path="/" element={<Login />} />
         {/* Additional routes here */}
-      </Switch>
-    </Router>
+      </Routes>
   );
 }
 
