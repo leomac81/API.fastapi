6f7210d634e66f65693b52a018eff3d394c0e702
Leo <leomacintyre81@gmail.com>
2023-10-12 20:58:38 +0100

    fixing imports

--- habits-app/src/App.js
+++ habits-app/src/App.js
@@ -1,42 +1,15 @@
-
-import React, { useState } from "react";
-import './App.css';
-import { Login } from "./components/LoginForm";
-import { Register } from "./components/RegisterForm";
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/LoginForm';
+import Homepage from './components/Homepage';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userEmail, setUserEmail] = useState('');
-  const [habits, setHabits] = useState([]);
-  const navigate = useNavigate();
-  const toggleForm = (formName) => {
-    if (formName === 'login') {
-      navigate('/login');
-    } else if (formName === 'register') {
-      navigate('/register');
-    }
-  }
-
-  const handleLogin = (email) => {
-    setIsLoggedIn(true);
-    setUserEmail(email);
-    navigate('/');
-  }
-  const handleLogout = () => {
-    localStorage.removeItem('access_token');
-    setIsLoggedIn(false);
-    navigate('/login');
-  }
-  const [lastUpdate, setLastUpdate] = useState(Date.now());
-
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-    </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/homepage" element={<Homepage />} />
+      </Routes>
   );
 }
 
--- habits-app/src/components/LoginForm.js
+++ habits-app/src/components/LoginForm.js
@@ -1,49 +1,44 @@
-import React, { useState } from "react";
+// src/components/Login.js
+import React, { useState } from 'react';
 import axios from 'axios';
 
-export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPass] = useState('');
-    const [errorMessage, setErrorMessage] = useState(null);
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        const formData = new FormData();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
         formData.append('username', email); // FastAPI expects 'username', not 'email'
         formData.append('password', password);
-
-        try {
-            const response = await axios.post('https://leoapi.xyz/api/login', formData);
-            
-            // If request is successful, store the access token in local storage.
-            localStorage.setItem('access_token', response.data.access_token);
-
-            // Notify the parent that login was successful
-            props.onLogin(email);
-
-        } catch (errorMessage) {
-            if (errorMessage.response && errorMessage.response.status === 403) {
-              setErrorMessage('Invalid credentials. Please try again.');
-            } else {
-              // handle other errors
-              console.error("An error occurred: ", errorMessage);
-            }}
-
+    try {
+      const response = await axios.post('https://leoapi.xyz/api/login', formData);
+      localStorage.setItem('token', response.data.access_token);
+      navigate('/homepage'); 
+    } catch (error) {
+      setError('Invalid credentials');
     }
+  };
 
-    return (
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
-                {errorMessage && <p className="error-message">{errorMessage}</p>}
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
-    )
-}
+  return (
+    <form onSubmit={handleLogin}>
+      {error && <p>{error}</p>}
+      <input 
+        type="email" 
+        placeholder="Email" 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input 
+        type="password" 
+        placeholder="Password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
