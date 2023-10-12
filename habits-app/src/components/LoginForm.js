174832665f982aace076e913e01f2b6cc04956ed
Leo <leomacintyre81@gmail.com>
2023-10-12 21:00:55 +0100

    adding homepage

--- habits-app/src/components/LoginForm.js
+++ habits-app/src/components/LoginForm.js
@@ -1,12 +1,14 @@
 // src/components/Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
+  const navigate = useNavigate();
+  
   const handleLogin = async (e) => {
     e.preventDefault();
     const formData = new FormData();
