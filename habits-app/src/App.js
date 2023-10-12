5301f78426fed1d8cb2dfcd5cdd0155ff82ca8ce
Leo <leomacintyre81@gmail.com>
2023-10-12 20:34:43 +0100

    adding new login page

--- habits-app/src/App.js
+++ habits-app/src/App.js
@@ -1,14 +1,66 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/LoginForm';
+
+import React, { useState } from "react";
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+import { Habits } from "./Habits";
+import { CreateHabit } from "./CreateHabits";
+import { TopBar } from './TopBar';
+import { HabitCompletionPage } from "./HabitCompletionPage";
+import  {FetchPublicHabits} from "./FetchPublicHabits"
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState('');
+  const [habits, setHabits] = useState([]);
+  const navigate = useNavigate();
+  const toggleForm = (formName) => {
+    if (formName === 'login') {
+      navigate('/login');
+    } else if (formName === 'register') {
+      navigate('/register');
+    }
+  }
+
+  const handleLogin = (email) => {
+    setIsLoggedIn(true);
+    setUserEmail(email);
+    navigate('/');
+  }
+  const handleLogout = () => {
+    localStorage.removeItem('access_token');
+    setIsLoggedIn(false);
+    navigate('/login');
+  }
+  const [lastUpdate, setLastUpdate] = useState(Date.now());
+
   return (
+    <div className="App">
+      {isLoggedIn && <TopBar title="Habit Tracker" onLogout={handleLogout} />}
       <Routes>
-        <Route path="/" element={<Login />} />
-        {/* Additional routes here */}
+        <Route path="/habit/gethabits/:habitId" element={<HabitCompletionPage />} />
+
+        <Route path="/login" element={<Login onFormSwitch={toggleForm} onLogin={handleLogin} />} />
+        <Route path="/register" element={<Register onFormSwitch={toggleForm} />} />
+        <Route path="/" element={
+          isLoggedIn ? (
+            <>
+              <div className="habits-layout">
+                <Habits habits={habits} setHabits={setHabits} isLoggedIn={isLoggedIn} userEmail={userEmail} setLastUpdate={setLastUpdate}/>
+                <FetchPublicHabits lastUpdate={lastUpdate}/>
+                <CreateHabit habits={habits} setHabits={setHabits} setLastUpdate={setLastUpdate}/>
+              </div>
+            </>
+          ) : (
+            currentForm === "login" ?
+              <Navigate to="/login" replace /> :
+              <Navigate to="/register" replace />
+          )
+        } />
       </Routes>
+    </div>
   );
 }
 
--- habits-app/src/components/LoginForm.js
+++ habits-app/src/components/LoginForm.js
@@ -1,44 +1,49 @@
-// src/components/Login.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from 'axios';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPass] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const formData = new FormData();
         formData.append('username', email); // FastAPI expects 'username', not 'email'
         formData.append('password', password);
-    try {
-      const response = await axios.post('https://leoapi.xyz/api/login', formData);
-      localStorage.setItem('token', response.data.access_token);
-      // Redirect or update UI as user is logged in
-    } catch (error) {
-      setError('Invalid credentials');
+
+        try {
+            const response = await axios.post('https://leoapi.xyz/api/login', formData);
+            
+            // If request is successful, store the access token in local storage.
+            localStorage.setItem('access_token', response.data.access_token);
+
+            // Notify the parent that login was successful
+            props.onLogin(email);
+
+        } catch (errorMessage) {
+            if (errorMessage.response && errorMessage.response.status === 403) {
+              setErrorMessage('Invalid credentials. Please try again.');
+            } else {
+              // handle other errors
+              console.error("An error occurred: ", errorMessage);
+            }}
+
     }
-  };
 
-  return (
-    <form onSubmit={handleLogin}>
-      {error && <p>{error}</p>}
-      <input 
-        type="email" 
-        placeholder="Email" 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input 
-        type="password" 
-        placeholder="Password" 
-        value={password} 
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
-};
-
-export default Login;
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">password</label>
+                <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
